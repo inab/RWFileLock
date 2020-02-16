60649305f9c46b025c865ba72a8241bc809923f4
José María Fernández <jose.m.fernandez@bsc.es>
2020-02-16 18:38:16 +0100

    Added official version 0.1.0 of the library

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import re
+import os
+import sys
+
+# In this way, we are sure we are getting
+# the installer's version of the library
+# not the system's one
+sys.path.insert(0,os.path.dirname(__file__))
+
+from RWFileLock import version as RWFileLock_version
+
+# Populating the long description
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+# Populating the install requirements
+requirements = []
+if os.path.exists('requirements.txt'):
+	with open('requirements.txt') as f:
+		egg = re.compile(r"#[^#]*egg=([^=&]+)")
+		for line in f.read().splitlines():
+			m = egg.search(line)
+			requirements.append(line  if m is None  else m.group(1))
+
+
+setuptools.setup(
+	name="RWFileLock",
+	version=RWFileLock_version,
+	author="José Mª Fernández",
+	author_email="jose.m.fernandez@bsc.es",
+	description="Readers / writers file lock helper class",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/inab/RWFileLock",
+	packages=setuptools.find_packages(),
+	install_requires=requirements,
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+		"Operating System :: OS Independent",
+	],
+)
